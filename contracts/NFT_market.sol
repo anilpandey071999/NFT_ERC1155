44c855f1ced2dabccd9ea74d4c0ac59f0000af72
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2022-02-15 19:10:03 +0530

    Nft

--- contracts/NFT_market.sol
+++ contracts/NFT_market.sol
@@ -3,33 +3,38 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "1.sol";
+import "./NFT.sol";
 
 contract MindDefMarketPlace{
     uint256 public totalNft = 0;
     mapping(uint256 => MarketItem) public idToMarketItem;
+    address public nftContract;
 
     struct MarketItem{
         uint256 marketId;
-        address nftContract;
+        uint256 nftID;
         address seller;
         address owner;
         bool sold;
     }
+    constructor(address _nftContract){
+        nftContract = _nftContract;
+    }
 
-    function addNftCollection(address nftContract) public {
-        idToMarketItem[totalNft] = MarketItem(
-            totalNft,
-            nftContract,
-            msg.sender,
-            msg.sender,
-            false
-        );
+    function addNftCollection(uint256 _nftID) public {
+        idToMarketItem[totalNft] = MarketItem({
+            marketId:totalNft,
+            nftID:_nftID,
+            seller:msg.sender,
+            owner:msg.sender,
+            sold:false
+        });
         // ERC1155(nftContract).setApprovalForAll(address(this),true);
     }
 
     function buy(uint256 _totalNft,uint256 nftId,uint256 amount)public{
-        MindDefnft(idToMarketItem[_totalNft].nftContract).buynft(idToMarketItem[_totalNft].owner,msg.sender,nftId,amount);
+        MindDefnft(nftContract).buynft(idToMarketItem[_totalNft].owner,msg.sender,nftId,amount);
+        idToMarketItem[_totalNft].owner = msg.sender;
     }
     
 }
