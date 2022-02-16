05364e99435d61ef25ead9becc8b32d3bf766f53
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2022-02-16 19:42:48 +0530

    NFT changes which were asked

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract MindDefnft is ERC1155{
     uint256 public goldCoin = 0;
     uint256 public nft = 1;
-    mapping(uint256=>uint256) public constOfnft;
-    mapping(address=>uint256) public ownerOfNft;
     address internal owner;
 
     constructor(address _owner)ERC1155("MindDef_nft"){
@@ -21,19 +19,4 @@ contract MindDefnft is ERC1155{
         nft++;
     }
 
-    function setPricenft(uint256 nftCost,uint256 tokenId) public {
-        require(msg.sender == owner,"Only for the Owner");
-        constOfnft[tokenId] = nftCost;
-    }
-
-    function buynft(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount
-    ) public {
-        require(constOfnft[id] <= amount,"Please pay listed amount");
-        safeTransferFrom(from, to, goldCoin, amount, "0x00");
-        safeTransferFrom(from, to, id, 1, "0x00");
-    }
 }
--- contracts/NFT_market.sol
+++ contracts/NFT_market.sol
@@ -9,32 +9,42 @@ contract MindDefMarketPlace{
     uint256 public totalNft = 0;
     mapping(uint256 => MarketItem) public idToMarketItem;
     address public nftContract;
+    address public owner;
 
     struct MarketItem{
-        uint256 marketId;
         uint256 nftID;
+        uint256 price;
         address seller;
-        address owner;
         bool sold;
     }
-    constructor(address _nftContract){
+    constructor(address _nftContract,address _owner){
         nftContract = _nftContract;
+        owner = _owner;
     }
 
     function addNftCollection(uint256 _nftID) public {
         idToMarketItem[totalNft] = MarketItem({
-            marketId:totalNft,
             nftID:_nftID,
+            price: 0,
             seller:msg.sender,
-            owner:msg.sender,
             sold:false
         });
-        // ERC1155(nftContract).setApprovalForAll(address(this),true);
     }
 
-    function buy(uint256 _totalNft,uint256 nftId,uint256 amount)public{
-        MindDefnft(nftContract).buynft(idToMarketItem[_totalNft].owner,msg.sender,nftId,amount);
-        idToMarketItem[_totalNft].owner = msg.sender;
+    function setPricenft(uint256 nftCost,uint256 _totalNft) public {
+        require(msg.sender == owner,"Only for the Owner");
+        idToMarketItem[_totalNft].price = nftCost;
     }
-    
+
+    function buynft(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        uint256 _totalNft
+    ) public {
+        require(idToMarketItem[_totalNft].price <= amount,"Please pay listed amount");
+        ERC1155(nftContract).safeTransferFrom(to,from,0, amount, "0x00");
+        ERC1155(nftContract).safeTransferFrom(from, to, id, 1, "0x00");
+    }
 }
